ee76212a890c802d6ba0f5f4ce87217dd2092c86
minecartchris <130516132+minecartchris@users.noreply.github.com>
2026-03-01 19:21:13 -0600

    Handle missing data in rednet message

--- prog2/black_jack_sp_v2.lua
+++ prog2/black_jack_sp_v2.lua
@@ -29,6 +29,7 @@ local function interactWithCard(userUUID, mode, money)
                 print("WTF!? I got a rednet message with no data!?")
                 print("Rebooting...")
                 sleep(5)
+                shell.run("reboot")
                 return 'error'
             end
 
